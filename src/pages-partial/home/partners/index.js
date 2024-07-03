2fa881eaee6e87441ca99c57734d4e97c48bcaed
Waris <warisahmed1293@gmail.com>
2024-07-04 01:53:52 +0500

    fix(ui): partner image responsiveness

--- src/pages-partial/home/partners/index.js
+++ src/pages-partial/home/partners/index.js
@@ -1,93 +1,39 @@
-import {
-  BoltShiftIcon,
-  FeatherDevIcon,
-  GlobalBankIcon,
-  LightBoxIcon,
-  SpheruleIcon,
-} from "@/assets/svgs";
 import Image from "next/image";
+
+const partners = [
+  { src: "/images/partners/venti.svg", alt: "Venti" },
+  { src: "/images/partners/bantam.svg", alt: "Bantam" },
+  { src: "/images/partners/chicagoscholars.svg", alt: "Chicago Scholars" },
+  { src: "/images/partners/uchicago.svg", alt: "UChicago" },
+  { src: "/images/partners/ground.svg", alt: "Ground" },
+];
+
+const RenderImages = () => {
+  return partners.map((partner, index) => (
+    <div key={index} className="lg:block inline">
+      <Image
+        src={partner.src}
+        alt={partner.alt}
+        height={90}
+        width={200}
+      />
+    </div>
+  ));
+};
 
 export default function PartnerSection() {
   return (
     <section className="bg-[#151515]">
       <div className="w-full max-w-[1440px] mx-auto bg-[#151515] border-y border-white/10">
-        <div className="md:py-[100px] md:px-[120px] py-8 overflow-x-hidden relative px-14 w-full flex flex-col gap-6 ">
+        <div className="md:py-16 md:px-[120px] py-4 overflow-x-hidden relative lg:px-14 px-2 w-full flex flex-col gap-2 ">
           <h5 className="text-center text-lg text-white">
             Partners in Innnovation
           </h5>
-          <div className="flex xl:justify-between gap-10 lg:animate-none animate-scroll items-center">
-            <div className="block">
-              <Image
-                src={"/images/partners/venti.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/bantam.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/chicagoscholars.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/uchicago.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/ground.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
+          <div className="flex xl:justify-between lg:gap-10 gap-2 lg:animate-none animate-scroll items-center">
+            <RenderImages />
           </div>
-          <div className="md:hidden flex justify-between md:gap-0 gap-10 md:animate-none animate-scroll items-center">
-            <div className="block">
-              <Image
-                src={"/images/partners/bantam.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/venti.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/uchicago.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/ground.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
-            <div className="block">
-              <Image
-                src={"/images/partners/chicagoscholars.svg"}
-                height={42}
-                width={170}
-              />
-            </div>
+          <div className="md:hidden flex justify-between md:gap-0 gap-2 md:animate-none animate-scroll items-center">
+            <RenderImages />
           </div>
         </div>
       </div>
