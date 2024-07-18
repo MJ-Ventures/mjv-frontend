ab72b4f577b08d392010096023f7c2c8957d67cc
Waris Ahmed <114222647+warisahmed1293@users.noreply.github.com>
2024-07-19 01:38:09 +0500

    feat(metatags): add dynamic meta tags functionality (#27)
    Co-authored-by: Waris <warisahmed1293@gmail.com>

--- src/app/case-studies/[id]/page.js
+++ src/app/case-studies/[id]/page.js
@@ -1,4 +1,47 @@
+import { baseURL } from "@/components/utils/endpoints";
 import CaseStudyPartial from "@/pages-partial/case-study";
+
+export async function generateMetadata({ params }) {
+  // read route params
+  const { id } = params;
+
+  // fetch data
+  const res = await fetch(`${baseURL}/api/get-single-case-study/id=${id}`);
+  const { caseStudy } = await res.json();
+  const { title, description, featuredImage } = caseStudy || {};
+
+  return {
+    // Basic SEO
+    title: title,
+    description: description,
+    canonical: "https://mjventures.org/",
+
+    // Open Graph / Facebook metadata
+    openGraph: {
+      title: title,
+      description: description,
+      url: `https://mjventures.org/`,
+      type: "website",
+      images: [
+        {
+          url: featuredImage,
+          width: 1200,
+          height: 630,
+          alt: "MJ Ventures",
+        },
+      ],
+    },
+
+    // Twitter Card metadata
+    twitter: {
+      card: "MJV",
+      site: "https://mjventures.org/",
+      title: title,
+      description: description,
+      images: [featuredImage],
+    },
+  };
+}
 
 export default function CaseStudy({ params }) {
   const { id } = params;
